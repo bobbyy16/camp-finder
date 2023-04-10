ec968644c536e373d450a9a234164bd58d6fbd1b
bobbyy16 <bobbyyyyy16@gmail.com>
2023-04-10 10:35:30 +0530

    Added db connection and setup for ejs

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path')
+const mongoose = require('mongoose')
 const app = express()
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/camp-finder');
+}
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Yelpcamp');
+    res.render('home');
 })
 
 app.listen(3000, () => {
