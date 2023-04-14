6e625dfbd505e2c39e1aca61ce77ffa8b0eafab2
bobbyy16 <bobbyyyyy16@gmail.com>
2023-04-14 14:53:19 +0530

    modified schemas.js

--- schemas.js
+++ schemas.js
@@ -1,4 +1,6 @@
+
 const Joi = require('joi');
+const { number } = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
@@ -9,3 +11,10 @@ module.exports.campgroundSchema = Joi.object({
         description: Joi.string().required()
     }).required()
 });
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+})
